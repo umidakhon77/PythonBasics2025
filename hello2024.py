32fef970e3b58bc7a8924ed875d7ca53f78fc8e9
Umidakhon <uma.mukhit@gmail.com>
2024-11-05 19:05:32 -0500

    lines

--- hello2024.py
+++ hello2024.py
@@ -2,3 +2,10 @@ print("Hello World")
 
 #line one was updated change
 #might change
+# 1. add TC-3,
+# 2. click file in left side
+# 3. write comment in left side at the bottom -> commit button(кнопка)
+#4.  press commit button
+#5. top one would be last changed, u should check at the top one and
+#6. if click this green one twice it show in the buttom two files
+#7. one file with changes would be with green color - mean
