0d55a37cb5c2cb1bf600034126bf3cc053f30621
Umidakhon <uma.mukhit@gmail.com>
2024-11-05 19:06:43 -0500

    lines

--- hello2024.py
+++ hello2024.py
@@ -6,6 +6,6 @@ print("Hello World")
 # 2. click file in left side
 # 3. write comment in left side at the bottom -> commit button(кнопка)
 #4.  press commit button
-#5. top one would be last changed, u should check at the top one and
+#5. top one would be last changed with u comments (kind name), u should check at the top one and
 #6. if click this green one twice it show in the buttom two files
 #7. one file with changes would be with green color - mean
