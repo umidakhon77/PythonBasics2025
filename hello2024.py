78fc60a41e6b9a7fa994d5af04411c484f6f17be
Umidakhon <uma.mukhit@gmail.com>
2025-02-18 19:25:08 -0500

    initialize git version control system for pythonBasic2024 project

--- hello2024.py
+++ hello2024.py
@@ -1,5 +1,5 @@
 print("Hello World")
-
+import sys
 #line one was updated change
 #might change
 # 1. add TC-3,
@@ -9,3 +9,17 @@ print("Hello World")
 #5. top one would be last changed with u comments (kind name), u should check at the top one and
 #6. if click this green one twice it show in the buttom two files
 #7. one file with changes would be with green color - mean
+
+name ='\n\t    john    doe'
+print(f'Welcome back, {name.strip().title()}! Glad to see u again!')
+#strip()  remove leading & trailing whitespace not between text
+
+print(name.replace('o', 'u'))
+print(f'Welcome back, {name.replace('o', 'u').strip().title()}! Glad to see u again!')
+
+
+msg='Hello!'
+print(msg)
+print(msg.replace('!', '?'))
+print(f"I'm using this file: {__file__}")
+print(f"I'm using this file: {sys.platform}")
